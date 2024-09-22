35ddff0e8a27910bd043c38b2ceb3c20c398e415
cody-bain <baincody@icloud.com>
2024-09-21 21:54:03 -0400

    updated theme settings

--- themes/anatole/assets/js/anatole-theme-switcher.js
+++ themes/anatole/assets/js/anatole-theme-switcher.js
@@ -17,7 +17,7 @@ const switchTheme = () => {
   const currThemeStyle = getStoredThemeStyle();
   switch (currThemeStyle) {
     case 'light':
-      setThemeStyle('light'); // change to 'dark' if you want enabled. I removed dark mode.
+      setThemeStyle('dark');
       break;
     case 'dark':
       setThemeStyle('light');
@@ -39,14 +39,12 @@ document.addEventListener(
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', switchTheme, false);
 
-// Uncomment if you want to get current user theme style.
-
-//const currThemeStyle = getStoredThemeStyle();
-//if (currThemeStyle) {
-//  setThemeStyle(currThemeStyle);
-//} else {
-//  const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-//  if (userPrefersDark) {
-//    setThemeStyle('dark');
-//  }
-//}
+const currThemeStyle = getStoredThemeStyle();
+if (currThemeStyle) {
+  setThemeStyle(currThemeStyle);
+} else {
+  const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  if (userPrefersDark) {
+    setThemeStyle('dark');
+  }
+}
